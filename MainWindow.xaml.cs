eebe5ac603e42a3d14fa36193a694e138ab1745d
Techno1919 <61212536+Techno1919@users.noreply.github.com>
2021-05-06 17:27:29 -0600

    Most windows are in place

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,12 +24,48 @@ namespace CasinoSim
         {
             InitializeComponent();
         }
-
+        
         private void BlackJack_Click(object sender, RoutedEventArgs e)
         {
             BlackJackWindow blackJackWindow = new BlackJackWindow();
             blackJackWindow.Show();
             Close();
         }
+
+        private void Craps_Click(object sender, RoutedEventArgs e)
+        {
+            CrapsWindow crapsWindow = new CrapsWindow();
+            crapsWindow.Show();
+            Close();
+        }
+
+        private void Roulette_Click(object sender, RoutedEventArgs e)
+        {
+            RouletteWindow rouletteWindow = new RouletteWindow();
+            rouletteWindow.Show();
+            Close();
+        }
+
+        private void Poker_Click(object sender, RoutedEventArgs e)
+        {
+            Poker pokerWindow = new Poker();
+            pokerWindow.Show();
+            Close();
+        }
+
+        private void Slots_Click(object sender, RoutedEventArgs e)
+        {
+            SlotsWindow slotsWindow = new SlotsWindow();
+            slotsWindow.Show();
+            Close();
+        }
+
+        private void Banking_Click(object sender, RoutedEventArgs e)
+        {
+            Banking_Window bankingWindow = new Banking_Window();
+            bankingWindow.Show();
+            Close();
+        }
+        
     }
 }
